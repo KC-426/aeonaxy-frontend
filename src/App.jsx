8f00ee17628c6a653a05967ce8c002ee7cef79f7
Kuldeep Chahar <kuldeepchahar426@gmail.com>
2024-04-11 19:41:38 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import React, { createContext } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
 import FindWork from "./pages/FindWork";
 
+export const AppContext = createContext();
+
 
 function App() {
+
   return (
     <div>
+          <AppContext.Provider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Signup />} />
@@ -15,6 +20,8 @@ function App() {
 
         </Routes>
       </BrowserRouter>
+      </AppContext.Provider>
+
     </div>
   );
 }
